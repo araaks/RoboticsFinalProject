913ddc2e69c1e745959995f65563be15826ccbf0
araaks <addisonraak@gmail.com>
2021-12-05 16:26:38 -0800

    Fixed Slider

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, Pressable, Button, Dimensions, Alert } from 'react-native';
-import { Video, AVPlaybackStatus } from 'expo-av';
-import VerticalSlider from 'rn-vertical-slider';
+// import { Video, AVPlaybackStatus } from 'expo-av';
+import BigSlider from 'react-native-big-slider';
 import axios from 'axios';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-export default class Home extends React.Component {
+export default class Home extends Component {
 
     constructor(props) {
         super(props);
+        this.state = { left: 0, right: 0 }
     }
 
     //testing to get data from backend server
@@ -23,9 +24,10 @@ export default class Home extends React.Component {
             .catch(function (error) {
                 console.log(error);
             });
-        }
+    }
 
     render() {
+
         return (
             <>
                 <View style={styles.container}>
@@ -48,27 +50,67 @@ export default class Home extends React.Component {
                     /> */}
 
                     {/* Vertical slider for tank controls of the robot */}
-                    <VerticalSlider
-                        leftSpeed={1}
+                    {/* <VerticalSlider
+                        value={1}
                         disabled={false}
                         min={0}
                         max={100}
                         onChange={(value) => {
-                            console.log("CHANGE", leftSpeed);
+                            console.log("CHANGE", value);
                         }}
                         onComplete={(value) => {
-                            console.log("COMPLETE", leftSpeed);
+                            console.log("COMPLETE", value);
                         }}
-                        width={50}
+                        width={100}
                         height={300}
                         step={1}
-                        borderRadius={5}
+                        borderRadius={10}
                         minimumTrackTintColor={"gray"}
                         maximumTrackTintColor={"tomato"}
                         showBallIndicator
                         ballIndicatorColor={"gray"}
                         ballIndicatorTextColor={"white"}
                     />
+                    <VerticalSlider
+                        value={1}
+                        disabled={false}
+                        min={0}
+                        max={100}
+                        onChange={(value) => {
+                            console.log("CHANGE", value);
+                        }}
+                        onComplete={(value) => {
+                            console.log("COMPLETE", value);
+                        }}
+                        width={100}
+                        height={300}
+                        step={1}
+                        borderRadius={10}
+                        minimumTrackTintColor={"gray"}
+                        maximumTrackTintColor={"tomato"}
+                        showBallIndicator
+                        ballIndicatorColor={"gray"}
+                        ballIndicatorTextColor={"white"}
+                    /> */}
+                    <BigSlider
+                        trackStyle={{ backgroundColor: 'rgb(255, 166, 102)' }}
+                        maximumValue={300}
+                        minimumValue={-300}
+                        value={this.state.left}
+                        onValueChange={left => {
+                            this.setState({ left })
+                            console.log(`LEFT: ${left}`)
+                        }} />
+
+                    <BigSlider
+                        trackStyle={{ backgroundColor: 'rgb(255, 166, 102)' }}
+                        maximumValue={300}
+                        minimumValue={-300}
+                        value={this.state.right}
+                        onValueChange={right => {
+                            this.setState({ right })
+                            console.log(`RIGHT: ${right}`)
+                        }} />
                 </View>
             </>
         )
@@ -78,11 +120,16 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ecf0f1',
+        // backgroundColor: '#ecf0f1',
         // alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         position: 'absolute',
-        marginLeft: 100
+        marginLeft: 100,
+        marginTop: 50,
+        flexDirection: 'row',
+        alignContent: 'space-between',
+        height: windowHeight - 90,
+        width: windowWidth - 200
     },
     buttonContainer: {
         flex: 1,
